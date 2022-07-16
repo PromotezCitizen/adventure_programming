2f40d596a1e9d3cd5b7ef41d3f40d48119634822
PromotezCitizen <gkstkdgus821@gmail.com>
2022-07-16 15:03:46 +0900

    함수 추가

--- lv.1/1-5/mem.py
+++ lv.1/1-5/mem.py
@@ -8,11 +8,15 @@ class NotNumErr(Exception):
     pass
 
 class Allocation():
-    __mem = []
-    def __init__(self):
-        self.__mem = [ 0 for _ in range(self.__allocArr())]
-        print(self.__mem, len(self.__mem))
-        None
+    __mem_size = 0
+    __alloc = []
+    __alloc_dict = {}
+    __alloc_flag = 0
+
+    def __init__(self, alloc_flag):
+        self.__mem_size= self.__allocArr()
+        self.__alloc_flag = alloc_flag
+        self.__alloc.append([0, self.__mem_size])
     
     def __allocArr(self):
         while True:
@@ -33,12 +37,71 @@ class Allocation():
 
         return arrsize
 
+    def myalloc(self):
+        mem_size = int(input("메모리 크기 입력 >> "))
+        if len(self.__alloc) == 0:
+            return -1
+        if mem_size > self.__alloc[0][1]:
+            return -1
 
-    def myalloc():
-        None
+        for idx, data in enumerate(self.__alloc):
+            mem_start = data[0]
+            self.__alloc_dict[mem_start] = mem_size
+            self.__alloc[idx] = [ mem_start + mem_size, data[1] - mem_size ]
+            None
 
-    def myfree():
-        None
+        while not self.__del():
+            None
+    
+        return mem_start
 
+    def __del(self):
+        for idx, data in enumerate(self.__alloc):
+            if data[1] == 0:
+                self.__alloc.pop(idx)
+                return False
+        return True
 
-mymem = Allocation()
+    def myfree(self, start):
+        try:
+            self.__alloc.append([start, self.__alloc_dict[start]])
+            del self.__alloc_dict[start]
+            while not self.__merge():
+                None
+            self.__alloc.sort(key=lambda x:x[0])
+        except:
+            print("Not available ADDRESS")
+    
+    def __merge(self):
+        for idx, data in enumerate(self.__alloc[:-2]):
+            if data[0] + data[1] == self.__alloc[idx+1][0]:
+                data[1] += self.__alloc[idx+1][1]
+                self.__alloc.pop(idx+1)
+                return False
+        return True
+
+    def printStat(self):
+        print(self.__alloc)
+        print(self.__alloc_dict)
+
+# 0 : first
+# 1 : best
+# 2 : worst
+allocation = []
+
+mymem = Allocation(0)
+
+mem_start = mymem.myalloc()
+if mem_start != -1: allocation.append(mem_start)
+
+mem_start = mymem.myalloc()
+if mem_start != -1: allocation.append(mem_start)
+
+mem_start = mymem.myalloc()
+if mem_start != -1: allocation.append(mem_start)
+
+print(allocation)
+
+mymem.myfree(int(input("free 주소 입력 >> ")))
+mymem.myfree(int(input("free 주소 입력 >> ")))
+mymem.printStat()
