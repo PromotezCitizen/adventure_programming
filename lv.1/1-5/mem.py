ab2514c04e6d9a53590999f2d37ecb32bb3f6385
PromotezCitizen <gkstkdgus821@gmail.com>
2022-07-16 21:34:39 +0900

    first fit complete

--- lv.1/1-5/mem.py
+++ lv.1/1-5/mem.py
@@ -41,17 +41,29 @@ class Allocation():
         mem_size = int(input("메모리 크기 입력 >> "))
         if len(self.__alloc) == 0:
             return -1
-        if mem_size > self.__alloc[0][1]:
-            return -1
+
+        exit_flag = 0
 
         for idx, data in enumerate(self.__alloc):
+            if mem_size > data[1]:
+                exit_flag += 1
+            
+        if exit_flag == len(self.__alloc):
+            return -1
+
+        # mem data append
+        for idx, data in enumerate(self.__alloc):
+            if mem_size > data[1]:
+                continue
             mem_start = data[0]
             self.__alloc_dict[mem_start] = mem_size
             self.__alloc[idx] = [ mem_start + mem_size, data[1] - mem_size ]
-            None
+            break
 
-        while not self.__del():
-            None
+        while True:
+            is_deleted = self.__del()
+            if is_deleted == True:
+                break
     
         return mem_start
 
@@ -65,43 +77,57 @@ class Allocation():
     def myfree(self, start):
         try:
             self.__alloc.append([start, self.__alloc_dict[start]])
-            del self.__alloc_dict[start]
-            while not self.__merge():
-                None
             self.__alloc.sort(key=lambda x:x[0])
+            del self.__alloc_dict[start]
+            while True:
+                is_merged = self.__merge()
+                if is_merged == True:
+                    break
         except:
-            print("Not available ADDRESS")
+            print("Not available ADDRESS - memory exception")
     
     def __merge(self):
-        for idx, data in enumerate(self.__alloc[:-2]):
+        for idx, data in enumerate(self.__alloc[:-1]):
             if data[0] + data[1] == self.__alloc[idx+1][0]:
-                data[1] += self.__alloc[idx+1][1]
+                self.__alloc[idx][1] += self.__alloc[idx+1][1]
                 self.__alloc.pop(idx+1)
                 return False
         return True
 
     def printStat(self):
-        print(self.__alloc)
-        print(self.__alloc_dict)
+        print("mem space : ", self.__alloc)
+        print_str = "mem usage : "
+        for k, v in sorted(self.__alloc_dict.items()):
+            print_str += "[" + str(k) + ": " + str(v) + "] "
+        print(print_str)
+
+def switches(allocation, switch):
+    if switch.lower() == "exit":
+        return "exit"
+    if switch.lower() == "malloc":
+        mem_start = mymem.myalloc()
+        if mem_start == -1:
+            print("Not available ADDRESS - malloc")
+        else:
+            allocation.append(mem_start)
+    if switch.lower() == "free":
+        free_space = int(input("free 주소 입력 >> "))
+        mymem.myfree(free_space)
+        try:
+            allocation.remove(free_space)
+        except:
+            None
+    return allocation
 
 # 0 : first
 # 1 : best
 # 2 : worst
-allocation = []
 
 mymem = Allocation(0)
+allocation = []
 
-mem_start = mymem.myalloc()
-if mem_start != -1: allocation.append(mem_start)
-
-mem_start = mymem.myalloc()
-if mem_start != -1: allocation.append(mem_start)
-
-mem_start = mymem.myalloc()
-if mem_start != -1: allocation.append(mem_start)
-
-print(allocation)
-
-mymem.myfree(int(input("free 주소 입력 >> ")))
-mymem.myfree(int(input("free 주소 입력 >> ")))
-mymem.printStat()
+while True:
+    allocation = switches(allocation, input("malloc, free, exit >> "))
+    if allocation == "exit":
+        break
+    mymem.printStat()
