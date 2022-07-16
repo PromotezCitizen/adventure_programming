f103ffa9ccabbd95b1771af3d292d56faa94e3a2
PromotezCitizen <gkstkdgus821@gmail.com>
2022-07-16 23:02:51 +0900

    best fit complete, dup functions to seperated function

--- lv.1/1-5/mem.py
+++ lv.1/1-5/mem.py
@@ -2,156 +2,160 @@
 # 1 : Alloced
 
 class NumRngErr(Exception): # 사용자 정의 에러
-    pass
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return "[RNG error] input value : " + self.msg
 
 class NotNumErr(Exception):
-    pass
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return "[NUM error] input value : " + self.msg
 
 class Allocation():
-    __mem_size = 0
+    # __mem_size = 0
     __alloc = []
     __alloc_dict = {}
     __alloc_flag = 0
 
     def __init__(self, alloc_flag):
-        self.__mem_size= self.__allocArr()
+        # self.__mem_size = self.__allocArr()
         self.__alloc_flag = alloc_flag
-        self.__alloc.append([0, self.__mem_size])
+        self.__alloc.append([0, self.__allocArr()])
     
     def __allocArr(self):
+        # arr size selection
         while True:
-            arrsize = input("배열 크기 입력 >> ")
+            arrsize = input("배열 크기 입력(10 이상) >> ")
             try:
                 if not arrsize.isdigit():
-                    raise NotNumErr
+                    raise NotNumErr(arrsize)
                 arrsize = int(arrsize)
                 if arrsize < 10:
-                    raise NumRngErr
+                    raise NumRngErr(arrsize)
                 break
 
-            except NotNumErr:
-                print("숫자를 입력해주세요.", sep=" ")
+            except NotNumErr as err:
+                print(err)
 
-            except NumRngErr:
-                print("10 이상을 입력해주세요. ", sep=" ")
+            except NumRngErr as err:
+                print(err)
 
         return arrsize
 
-    def firstfit(self):
-        mem_size = int(input("메모리 크기 입력 >> "))
-        if len(self.__alloc) == 0:
-            return -1
-
-        exit_flag = 0
-
-        for idx, data in enumerate(self.__alloc):
-            if mem_size > data[1]:
-                exit_flag += 1
-            
-        if exit_flag == len(self.__alloc):
-            return -1
-
+    def firstfit(self, mem_size):
         # mem data append
         for idx, data in enumerate(self.__alloc):
             if mem_size > data[1]:
                 continue
-            mem_start = data[0]
-            self.__alloc_dict[mem_start] = mem_size
-            self.__alloc[idx] = [ mem_start + mem_size, data[1] - mem_size ]
+            mem_start = self.__memAppend(idx, mem_size)
+            # mem_start = data[0]
+            # self.__alloc_dict[mem_start] = mem_size
+            # self.__alloc[idx] = [ mem_start + mem_size, data[1] - mem_size ]
             break
-
-        while True:
-            is_deleted = self.__del()
-            if is_deleted == True:
-                break
 
         return mem_start
 
-    def bestfit(self):
-        mem_size = int(input("메모리 크기 입력 >> "))
-        if len(self.__alloc) == 0:
-            return -1
-
-        exit_flag = 0
-
-        for idx, data in enumerate(self.__alloc):
-            if mem_size > data[1]:
-                exit_flag += 1
-            
-        if exit_flag == len(self.__alloc):
-            return -1
-
-        # mem data append
-        for idx, data in enumerate(self.__alloc):
-            if mem_size > data[1]:
-                continue
-            mem_start = data[0]
-            self.__alloc_dict[mem_start] = mem_size
-            self.__alloc[idx] = [ mem_start + mem_size, data[1] - mem_size ]
-            break
-
-        while True:
-            is_deleted = self.__del()
-            if is_deleted == True:
-                break
-        
-        return mem_start
-
-    def worstfit(self):
-        mem_size = int(input("메모리 크기 입력 >> "))
-        if len(self.__alloc) == 0:
-            return -1
-
-        exit_flag = 0
-
+    def bestfit(self, mem_size):
+        # select which memory use
+        self.__alloc.sort(key=lambda x:x[0], reverse=True)
         self.__alloc.sort(key=lambda x:x[1], reverse=True)
-        print(self.__alloc)
 
-        for _, data in enumerate(self.__alloc):
-            if mem_size > data[1]:
-                exit_flag += 1
-            
-        if exit_flag == len(self.__alloc):
-            return -1
-
-        # mem data append
-        mem_start = self.__alloc[0][0]
-        self.__alloc_dict[mem_start] = mem_size
-        self.__alloc[0] = [ mem_start + mem_size, self.__alloc[0][1] - mem_size ]
-
-        while True:
-            is_deleted = self.__del()
-            if is_deleted == True:
+        idx_temp = 0
+        for idx ,data in enumerate(self.__alloc):
+            if data[1] >= mem_size:
+                idx_temp = idx
+            else:
                 break
         
+        # mem data append
+        mem_start = self.__memAppend(idx_temp, mem_size)
+        # mem_start = self.__alloc[idx_temp][0]
+        # self.__alloc_dict[mem_start] = mem_size
+        # self.__alloc[idx_temp] = [ mem_start + mem_size, self.__alloc[idx_temp][1] - mem_size ]
+
+        return mem_start
+
+    def worstfit(self, mem_size):
+        # mem data append
+        self.__alloc.sort(key=lambda x:x[1], reverse=True)
+
+        mem_start = self.__memAppend(0, mem_size)
+        # mem_start = self.__alloc[0][0]
+        # self.__alloc_dict[mem_start] = mem_size
+        # self.__alloc[0] = [ mem_start + mem_size, self.__alloc[0][1] - mem_size ]
+        
+        return mem_start
+
+    def __memAppend(self, idx, mem_size):
+        mem_start = self.__alloc[idx][0]
+        self.__alloc_dict[mem_start] = mem_size
+        self.__alloc[idx] = [ mem_start + mem_size, self.__alloc[idx][1] - mem_size ]
         return mem_start
 
     def myalloc(self):
+        mem_size = int(input("메모리 크기 입력 >> "))
+        # if not available in memory...
+        # return error: -1
+        if len(self.__alloc) == 0:
+            return -1
+
+        # if not fit mem size in memory...
+        # return error: -1
+        exit_flag = 0
+        for _, data in enumerate(self.__alloc):
+            if mem_size > data[1]:
+                exit_flag += 1
+
+        if exit_flag == len(self.__alloc):
+            return -1
+
+        # mem append method
         if self.__alloc_flag == 0: # fitst fit
-            return self.firstfit()
-        if self.__alloc_flag == 1: # best fit, not work
-            return self.bestfit()
+            mem_start = self.firstfit(mem_size)
+        if self.__alloc_flag == 1: # best fit
+            mem_start = self.bestfit(mem_size)
         if self.__alloc_flag == 2: # worst fit
-            return self.worstfit()
+            mem_start = self.worstfit(mem_size)
+
+        # delete mem
+        while True:
+            is_deleted = self.__del()
+            if is_deleted == True:
+                break
+
+        return mem_start
 
     def __del(self):
+        # delete mem data if left mem size === 0
+        # True  : mem size === 0 block Not in mem data
+        # False : mem size === 0 block in mem data
         for idx, data in enumerate(self.__alloc):
             if data[1] == 0:
                 self.__alloc.pop(idx)
                 return False
         return True
 
-    def myfree(self, start):
+    def myfree(self):
+        if len(self.__alloc_dict) == 0:
+            print("Not available ADDRESS - all is free")
+            return
         try:
-            self.__alloc.append([start, self.__alloc_dict[start]])
+            free_mem = int(input("free 주소 입력 >> "))
+            self.__alloc.append([free_mem, self.__alloc_dict[free_mem]])
             self.__alloc.sort(key=lambda x:x[0])
-            del self.__alloc_dict[start]
+            del self.__alloc_dict[free_mem]
             while True:
                 is_merged = self.__merge()
                 if is_merged == True:
                     break
         except:
             print("Not available ADDRESS - memory exception")
+
+        return free_mem
     
     def __merge(self):
         for idx, data in enumerate(self.__alloc[:-1]):
@@ -178,15 +182,14 @@ def switches(allocation, switch):
         else:
             allocation.append(mem_start)
     if switch.lower() == "free":
-        free_space = int(input("free 주소 입력 >> "))
-        mymem.myfree(free_space)
+        free_space = mymem.myfree()
         try:
             allocation.remove(free_space)
         except:
             None
     return allocation
 
-mymem = Allocation(2)
+mymem = Allocation(0)
 allocation = []
 
 while True:
