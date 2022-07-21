1d09f3efa5d2d93516bf50ca54470abea1a1aea2
PromotezCitizen <gkstkdgus821@gmail.com>
2022-07-21 18:12:39 +0900

    전화 예약 시스템 구현

--- lv.2/2-4/taffy.py
+++ lv.2/2-4/taffy.py
@@ -37,13 +37,24 @@ def run():
     service_time_left = 0
     custom_idx = 1
     turn = 1
-
+    is_service = 0
     # run
     # 영업시간 직전까지 고객 처리. 이후에 큐에 있는 고객은 무시
+    # 1. 전화 예약을 어떻게 구현할 것인가? - 완료
+    #   -- 순서 --
+    #   1) 예약이 오면 queue에 저장
+    #   2) 다른 인원 및 현재 인원 대기시간 1 증가(전화로 인한 대기)
+    #       2-1) 다른 인원 : 간단히 addWaitTimes 이용
+    #       2-2) 현재 서비스 인원 : 배열 -1인덱스 이용
+    #           구현이 힘드므로 들어올때는 
+    # 2. 스레드를 어떻게 사용할 것인가?
+    #   기본 queue를 선언하고 메모리 공유를 이용해야하는가?
+    # 3. GUI는 개씨발이다
     while turn <= MAX_TIME:
         turn += 1
         
-        if random.randint(1,4) == 4:
+        service = random.randint(1,4)
+        if service == 4:
             if not waiter_queue.isFull():
                 print('now turn : ', turn)
                 service_time = random.randint(1, 10)
@@ -54,11 +65,25 @@ def run():
                 print('\t%2d customer append(time:%3d)' % (custom_idx, turn))
                 waiter_queue.print()
                 custom_idx += 1
+        elif service == 3: # 전화 예약
+            if not waiter_queue.isEmpty():
+                waiter_queue.addWaitTimes()
+                if is_service == 1:
+                    service_time_arr[-1][-1] += 1
+            if not waiter_queue.isFull():
+                print('now turn : ', turn)
+                service_time = random.randint(1, 10)
+                waiter_queue.enqueue([custom_idx, service_time, turn, 0])
+                print('\t%2d booking customer append(time:%3d)' % (custom_idx, turn))
+                waiter_queue.print()
+                custom_idx += 1
+                continue
 
         if waiter_queue.isEmpty():
             continue
-
+    
         if service_time_left == 0:
+            is_service = 0
             print('now turn : ', turn)
             temp = waiter_queue.dequeue()
             temp.insert(-1, turn) # service_start time
@@ -66,7 +91,9 @@ def run():
             service_time_arr.append(temp)
             print("\tqueue removed :: customer %2d, arrival %3d, start %3d, wait %2d, service %2d"
                 % (temp[0], temp[2], temp[3], temp[4], temp[1]))
+            waiter_queue.print()
         else:
+            is_service = 1
             waiter_queue.addWaitTimes()
             service_time_left -= 1
     
