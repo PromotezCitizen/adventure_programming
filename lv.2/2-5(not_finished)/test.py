133ec48069ecbe26f1edfc418f4a1dec5f7392c7
PromotezCitizen <gkstkdgus821@gmail.com>
2022-07-27 13:06:40 +0900

    포맷 전 백업

--- lv.2/2-5(not_finished)/test.py
+++ lv.2/2-5(not_finished)/test.py
@@ -3,22 +3,25 @@ from pynput import keyboard
 def spin():
     print('start')
     while pause:
-        None
+        with keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
     print('end')
 
 def on_press(key):
     try:
         if key.char == 'a':
+            global pause
+            print('pressed a', pause)
             pause = False
             return True
     except:
         None
 
     if key == keyboard.Key.up:
+        print('pressed up')
         return False
 
 if __name__ == "__main__":
     pause = True
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
+
     spin()
