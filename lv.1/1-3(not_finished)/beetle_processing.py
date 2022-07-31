464b981973f632ddc1e80c7a1674c9ec0ab29d3e
PromotezCitizen <gksktdgus821@gmail.com>
2022-07-31 15:52:05 +0900

    프로세스 생성 함수 변경(딱정벌레 수 추가)

--- lv.1/1-3(not_finished)/beetle_processing.py
+++ lv.1/1-3(not_finished)/beetle_processing.py
@@ -2,13 +2,13 @@ import time
 import multiprocessing as mp
 from beetle import Beetle
 
-def useProcessing(map_size, splited):
+def useProcessing(map_size, splited, num_roach):
     return_dict = mp.Manager().dict()
     jobs = []
 
     start = time.perf_counter()
     for idx, calc_range in enumerate(splited):
-        p = mp.Process(target=worker, args=(idx, map_size, calc_range, return_dict))
+        p = mp.Process(target=worker, args=(idx, map_size, calc_range, return_dict, num_roach))
         jobs.append(p)
         p.start()
     
@@ -22,7 +22,7 @@ def useProcessing(map_size, splited):
     end = time.perf_counter()
     return beetle_result_list, end-start
 
-def worker(idx, map_size, calc_range, return_dict):
+def worker(idx, map_size, calc_range, return_dict, num_roach):
     beetle = Beetle(map_size, calc_range)
     nqueen_result = beetle.start()
     return_dict[idx] = nqueen_result
