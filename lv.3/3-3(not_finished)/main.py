6e9513487f6c969131c003a5e7d7171ff823dc4a
PromotezCitizen <gkstkdgus821@gmail.com>
2022-08-01 17:32:58 +0900

    흐름 제어 함수 템플릿만 적용

--- lv.3/3-3(not_finished)/main.py
+++ lv.3/3-3(not_finished)/main.py
@@ -2,14 +2,18 @@ class UCodeInterpreter():
     def __init__(self, folder, file):
         self.__folder_path = folder
         self.__file_path = file
+        self.__ucode = None
         self.__proc_starts = None
         self.__label_starts = None
+        self.__stack = []
+        self.__mem = []
 
     def run(self):
-        ucode = self._getUCODEData()
-        self.__proc_starts = self._getStartPosition(ucode, 'proc')
-        self.__label_starts = self._getStartPosition(ucode, 'nop')
+        self.__ucode = self._getUCODEData()
+        self.__proc_starts = self._getStartPosition(self.__ucode, 'proc')
+        self.__label_starts = self._getStartPosition(self.__ucode, 'nop')
         print(self.__proc_starts, self.__label_starts)
+        self._runUcode()
         return None
 
     def _getUCODEData(self):
@@ -57,25 +61,37 @@ class UCodeInterpreter():
                 func_start[line[0]] = idx
         return func_start
 
+    def _runUcode(self):
+        turn = 0
+        idx = 0
+        ret_pos = None
+        while turn < 200:
+            try:
+                data = self.__ucode[idx]
+            except:
+                break
+
+            print('turn-%4d(%4d)' % (turn, idx), data)
+            if 'call' in data:
+                try:
+                    ret_pos = idx
+                    idx = self.__proc_starts[data[1]]
+                except:
+                    None
+            elif 'ujp' in data:
+                idx = self.__label_starts[data[1]]
+            elif 'proc' in data:
+                idx = self.__proc_starts[data[0]]
+            elif 'nop' in data:
+                idx = self.__label_starts[data[0]]
+            elif 'ret' in data:
+                idx = ret_pos
+
+            idx += 1
+            turn += 1
 
 # interpreter = UCodeInterpreter('레벨3.3 UCode테스트/', 'test1.uco')
 # interpreter.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -125,34 +141,99 @@ def getStartPosition(ucode, name):
             func_start[line[0]] = idx
     return func_start
 
-ucode = getUCODEData()
-proc_starts = getStartPosition(ucode, 'proc')
-label_starts = getStartPosition(ucode, 'nop')
+def start():
+    # 프로그램 구성
+    def bgn(line):
+        None
 
-turn = 0
-idx = 0
-ret_pos = None
-while turn < 200:
-    try:
-        data = ucode[idx]
-    except:
-        break
+    def sym(line):
+        None
 
-    print('turn-%4d(%4d)' % (turn, idx), data)
-    if 'call' in data:
-        try:
-            ret_pos = idx
-            idx = proc_starts[data[1]]
-        except:
+    # 흐름 제어
+    def njp(line, idx):
+        if line[0] == 'fjp':
             None
-    elif 'ujp' in data:
-        idx = label_starts[data[1]]
-    elif 'proc' in data:
-        idx = proc_starts[data[0]]
-    elif 'nop' in data:
-        idx = label_starts[data[0]]
-    elif 'ret' in data:
-        idx = ret_pos
+            # flag = stack.pop()
+            # if flag == False:
+            #     idx = label_starts[data[1]]
+        elif line[0] == 'tjp':
+            None
+            # flag = stack.pop()
+            # if flag == True:
+            #     idx = label_starts[data[1]]
+        elif line[0] == 'ujp':
+            idx = label_starts[line[1]]
 
-    idx += 1
-    turn += 1
+        return idx
+
+    # 이항 연산 - eval 사용
+    def binaryOperation(operator):
+        None
+        #stack.append(eval('{0}{1}{2}'.format(stack[-2], operator, stack[-1])))
+
+
+    ucode = getUCODEData()
+    proc_starts = getStartPosition(ucode, 'proc')
+    label_starts = getStartPosition(ucode, 'nop')
+
+    turn = 0
+    idx = 0
+    ret_pos = None
+
+    global stack
+    global mem
+
+    while turn < 200: # data[0] != 'end'
+        try:
+            data = ucode[idx]
+        except:
+            break
+
+        print('turn-%4d(%4d)' % (turn, idx), data)
+        if 'call' in data:
+            try:
+                ret_pos = idx
+                idx = proc_starts[data[1]]
+            except:
+                None
+
+        elif data[0].find('jp') > 0 :
+            idx = njp(data, idx)
+
+        elif 'proc' in data:
+            idx = proc_starts[data[0]]
+
+        elif 'nop' in data:
+            idx = label_starts[data[0]]
+
+        elif 'ret' in data:
+            idx = ret_pos
+
+        idx += 1
+        turn += 1
+
+
+
+stack = []
+mem = []
+operators = [
+    {'add': '+'},
+    {'sub': '-'},
+    {'mult': '*'},
+    {'div': '/'},
+    {'mod': '%'},
+    {'gt': '<'},
+    {'lt': '>'},
+    {'ge': '<='},
+    {'le': '>='},
+    {'eq': '=='},
+    {'ne': '!='},
+    {'and': '&&'},
+    {'or': '||'}
+]
+start()
+
+# num1 = 3
+# num2 = 10
+# calc_data = '{0}{1}{2}'.format(num1, '+', num2)
+# print(eval(calc_data))
