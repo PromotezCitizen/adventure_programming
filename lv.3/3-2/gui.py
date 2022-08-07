afa39d391f43d6469c09a60dee2b0d1f31d7a894
PromotezCitizen <gkstkdgus821@gmail.com>
2022-08-07 14:00:00 +0900

    테스트용 print문 제거(1)

--- lv.3/3-2/gui.py
+++ lv.3/3-2/gui.py
@@ -4,14 +4,11 @@ import os
 from main_dialog import GUI
 
 if __name__ == "__main__":
-
-    print(mp.current_process().name, __name__)
     mp.freeze_support()
     # https://github.com/pyinstaller/pyinstaller/issues/3957#issuecomment-674579877
     # Pyinstaller multiprocessing name of process is always "MainProcess" 해결
     if 'main_started' not in os.environ:
         os.environ['main_started'] = ''
-        print(mp.current_process().name, __name__)
         GUI().run() # 인스턴스 생성 없이 바로 실행
         os.system('pause')
 
